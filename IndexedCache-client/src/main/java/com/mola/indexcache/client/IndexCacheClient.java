4b6df5e04ea1d829677a1c3898c2f48d8846b2dd
liuge.xzy <liuge.xzy@cainiao.com>
2021-09-19 23:31:43 +0800

    init

---
+++ IndexedCache-client/src/main/java/com/mola/indexcache/client/IndexCacheClient.java
@@ -0,0 +1,9 @@
+package com.mola.indexcache.client;
+
+/**
+ * @Author: liuge.xzy
+ * @Description: 索引client统一门面
+ * @DateTime: 2021/9/19 11:24 下午
+ **/
+public class IndexCacheClient {
+}
---
+++ IndexedCache-common/src/main/java/com/mola/indexcache/common/annotation/IndexedCache.java
@@ -0,0 +1,9 @@
+package com.mola.indexcache.common.annotation;
+
+/**
+ * @Author: liuge.xzy
+ * @Description: 注解在类上，表示一个索引缓存
+ * @DateTime: 2021/9/19 11:30 下午
+ **/
+public @interface IndexedCache {
+}
